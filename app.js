6a9652758ec69e3e1d9c9df3702a4a4053e94107
luisgonzalez2001 <luis.gonzalez4737@alumnos.udg.mx>
2024-07-31 21:05:22 -0600

    Se añadieron las funciones de eliminar y editar tareas en el task

--- app.js
+++ app.js
@@ -8,4 +8,45 @@ taskForm.addEventListener("submit", (event) => {
     const task = taskInput.value;
 
     console.log(task)
+
+    if(task) {
+        taskList.append(createTaskElement(task));
+        taskInput.value = null;
+    }
 })
+
+function createTaskElement(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    li.append(createButton("delete-btn"), createButton("edit-btn"));
+    return li;
+}
+
+function createButton(className) {
+    const button = document.createElement("span");
+    button.className = className;
+    return button;
+}
+
+taskList.addEventListener("click", (event) => {
+    console.log(event.target);
+
+    if (event.target.classList.contains("delete-btn")){
+        deleteTask(event.target.parentElement);
+    } else if (event.target.classList.contains("edit-btn")){
+        editTask(event.target.parentElement);
+    }
+})
+
+function deleteTask(task) {
+    if (confirm("¡Estas seguro que deseas eliminar la tarea?")){
+        task.remove();
+    }
+}
+
+function editTask(task) {
+    const newTask = prompt("Edita la tarea:", task.firstChild.textContent);
+    if (newTask != null){
+        task.firstChild.textContent = newTask;
+    }
+}
