6bca418d5c58cb3141f53e2897fda97afbba6d1e
luisgonzalez2001 <luis.gonzalez4737@alumnos.udg.mx>
2024-08-02 20:09:50 -0600

    Se finalizo el proyecto 100% funcional, ya es posible editar y eliminar tareas, cambiar el tema y todo esto con persistencia en localStorage

--- app.js
+++ app.js
@@ -2,16 +2,42 @@ const taskForm = document.getElementById("taskForm");
 
 const taskList = document.getElementById("taskList");
 
+const themeToogleButton = document.getElementById("themeToogle");
+
+const currentTheme = localStorage.getItem("theme");
+
+loadTasks();
+
+if(currentTheme === "dark") {
+    document.body.classList.add("dark-theme")
+}
+
 taskForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const taskInput = document.getElementById("taskInput");
     const task = taskInput.value;
-
-    console.log(task)
-
     if(task) {
         taskList.append(createTaskElement(task));
+        saveTasksInLocalStorage(task)
         taskInput.value = null;
+    }
+})
+
+
+themeToogleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    const theme = document.body.classList.contains("dark-theme")
+        ? "dark"
+        : "light";
+    localStorage.setItem("theme", theme);
+});
+
+taskList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-btn")){
+        deleteTask(event.target.parentElement);
+    } else if (event.target.classList.contains("edit-btn")){
+        editTask(event.target.parentElement);
     }
 })
 
@@ -28,20 +54,12 @@ function createButton(className) {
     return button;
 }
 
-taskList.addEventListener("click", (event) => {
-    console.log(event.target);
-
-    if (event.target.classList.contains("delete-btn")){
-        deleteTask(event.target.parentElement);
-    } else if (event.target.classList.contains("edit-btn")){
-        editTask(event.target.parentElement);
-    }
-})
 
 function deleteTask(task) {
     if (confirm("¡Estas seguro que deseas eliminar la tarea?")){
         task.remove();
     }
+    updateLocalStorage();
 }
 
 function editTask(task) {
@@ -49,4 +67,25 @@ function editTask(task) {
     if (newTask != null){
         task.firstChild.textContent = newTask;
     }
+    updateLocalStorage();
+}
+
+function saveTasksInLocalStorage(task) {
+    const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks" || "[]"));
+    tasks.forEach(task => {
+        taskList.appendChild(createTaskElement(task));
+    });
+}
+
+function updateLocalStorage() {
+    const tasks = Array.from(taskList.querySelectorAll("li")).map(
+        (li) => li.firstChild.textContent
+    );
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
